cd5543567927da4b2c02b1cb8cfa21e4e3b19af9
loutchoa <francois@di.ku.dk>
2022-01-27 00:36:35 +0100

    Added a parametrized FISTA/Gradient projection which should be used for segmentation labels smoothing. Some clean up in projections.py

--- python/fistaTV.py
+++ python/fistaTV.py
@@ -14,12 +14,18 @@ Date: 08-2016
 Small stuffs: cleaning of the code and Python, August 2021
 """
 
-
 import numpy as np
 from graddiv import grad2, grad3, div2, div3
 from operator import mul
 from functools import reduce
-from scalespace import gaussian_scalespace
+from projectors import (
+    project_on_field_of_balls,
+    project_on_field_of_Frobenius_balls
+)
+from math import sqrt
+import numbers
+# from scalespace import gaussian_scalespace
+
 
 def Pc(x, xmin, xmax):
     """
@@ -29,7 +35,7 @@ def Pc(x, xmin, xmax):
     np.place(x, x > xmax, xmax)
 
 
-def Pp(psi, l=1.0):
+def project_on_field_of_balls(psi, l=1.0):
     """
     project a vector field on the field of radius l balls,
     i.e. each vector v of the field is projected in a radius l ball. If 
@@ -38,7 +44,7 @@ def Pp(psi, l=1.0):
     
     :param psi: numpy array.
         the vector field to be projected
-    :param lambda: float. Then radius of each ball.
+    :param l: float. Then radius of each ball.
     :return: psi projected
     """
 
@@ -47,52 +53,52 @@ def Pp(psi, l=1.0):
     rdim = reduce(mul, psishape[:-1], 1)
     psi.shape = (rdim, vdim)
 
-    psinorm = np.sqrt(np.sum(psi**2, axis=1))
-    psinorm = (psinorm/l)*(psinorm > l) + np.ones_like(psinorm)*(psinorm <= l)
-    #np.place(psinorm, psinorm <= 1.0, 1.0)
+    psinorm = np.sqrt(np.sum(psi ** 2, axis=1))
+    psinorm = (psinorm / l) * (psinorm > l) + np.ones_like(psinorm) * (psinorm <= l)
+    # np.place(psinorm, psinorm <= 1.0, 1.0)
     for i in range(vdim):
-        psi[:,i] /= psinorm
+        psi[:, i] /= psinorm
     psi.shape = psishape
     return psi
 
 
+def fistaTV(b, l, n, xmin=0.0, xmax=float("inf")):
+    """Compute the l-proximal of the TV-norm with range constraints.
 
-
-
-def fistaTV(b, l, n, xmin=0.0, xmax = float("inf")):
-    """
-    computes
     .. math::
     argmin_x \|x-b\|^2 + 2l\|Dx\|,\quad xmin <= x  <= xmax
-    
-    
+
     i.e. the proximal $prox_f(b)
     where f = l*\|D.\| + i_B(.) with
-    
-             
+
     i_B(x) = +infty if one of the components of $x$ does not
     satisfy xmin <= x_i <= xmax, and i_B(x) = 0 otherwise,
-    i.e., a identical bounds box constraint
-    
-    :param b: numpy float32 array.
+    i.e.,  identical bounds box constraint
+
+    Parameters:
+    ----------
+    b : numpy float32 array.
         image to be denoised, 2 or 3D array.
-    :param l: float.
+    l : float.
         regularization weight
-    :param n: integer.
+    n : integer.
         number of iterations
-    xmin: float.
+    xmin : float.
         min value of x-entry, default 0
-    xmax: float.
-        max value of x-entry, default +infty.
-        
-    :return x: regularized image
+    xmax : float.
+        max value of x-entry, default +inf.
+
+    Return:
+    -------
+    x : numpy array
+        regularized image
     """
     # Have we active box constraints on solution values?
     bc = False if (xmin, xmax) == (-float("inf"), float("inf")) else True
     k = len(b.shape)
     grad = grad2 if k == 2 else grad3
     div = div2 if k == 2 else div3
-    tau = 1./(8*l) if k == 2 else 1.0/(12*l)
+    tau = 1. / (8 * l) if k == 2 else 1.0 / (12 * l)
 
     s = b.shape + (k,)
     r = np.zeros(s, dtype="float32")
@@ -102,19 +108,19 @@ def fistaTV(b, l, n, xmin=0.0, xmax = float("inf")):
 
     for i in range(n):
         # print i
-        a = b - l*div(r)
+        a = b - l * div(r)
         if bc:
             Pc(a, xmin, xmax)
 
-        pn = r - tau*grad(a)
-        Pp(pn)
+        pn = r - tau * grad(a)
+        project_on_field_of_balls(pn)
 
-        tn = 0.5*(1.0 + np.sqrt(1.0 + 4*tc**2))
-        r = pn + ((tc-1)/tn)*(pn - pc)
+        tn = 0.5 * (1.0 + np.sqrt(1.0 + 4 * tc ** 2))
+        r = pn + ((tc - 1) / tn) * (pn - pc)
         tc = tn
         pc = pn
 
-    x = b - l*div(pc)
+    x = b - l * div(pc)
     if bc:
         Pc(x, xmin, xmax)
 
@@ -131,38 +137,43 @@ def fistaTV_weighted(b, l, W, n, omega=None, xmin=0.0, xmax=float("inf")):
     i.e. the proximal $prox_f(b)
     where f = l*\|DW.\| + i_B(.) with
 
-    i_B(x) = +infty if one of the components of $x$ does not
+    i_B(x) = +infinity if one of the components of x does not
     satisfy xmin <= x_i <= xmax, and i_B(x) = 0 otherwise,
     i.e., a identical bounds box constraint, while W is a
     diagonal matrix.
 
-    :param b: numpy float32 array.
+    Parameters:
+    ----------
+    b : numpy float32 array.
         image to be denoised, 2 or 3D array.
-    :param l: float.
+    l : float.
         regularization weight
-    :param W: numpy float32 array
+    W : numpy float32 array
         weight/space deformation array.
-    :param n: integer.
+    n : integer.
         number of iterations.
-    :param omega: float or None, optional.
+    omega : float or None, optional.
         The sup norm of W. if set to None, this is evaluated.
-    xmin: float. Optional
+    xmin : float. Optional
         min value of x-entry. The default value is 0.
-    xmax: float, optional.
+    xmax : float, optional.
         max value of x-entry. The default value is +INF
 
-    :return x: regularized image
+    Return:
+    ------
+    x : numpy array
+        regularized image
     """
     # Have we active box constraints on solution values?
     bc = False if (xmin, xmax) == (-float("inf"), float("inf")) else True
     # omega should be the sup norm of W.*W
     if omega is None:
-        omega = (W**2).max()
+        omega = (W ** 2).max()
 
     k = len(b.shape)
     grad = grad2 if k == 2 else grad3
     div = div2 if k == 2 else div3
-    tau = 1./(8*omega*l) if k == 2 else 1.0/(12*omega*l)
+    tau = 1. / (8 * omega * l) if k == 2 else 1.0 / (12 * omega * l)
 
     s = b.shape + (k,)
     r = np.zeros(s, dtype="float32")
@@ -172,27 +183,248 @@ def fistaTV_weighted(b, l, W, n, omega=None, xmin=0.0, xmax=float("inf")):
 
     for i in range(n):
         # print i
-        a = b - l*W*div(r)
+        a = b - l * W * div(r)
         if bc:
             Pc(a, xmin, xmax)
 
-        pn = r - tau*grad(W*a)
-        Pp(pn)
+        pn = r - tau * grad(W * a)
+        project_on_field_of_balls(pn)
 
-        tn = 0.5*(1.0 + np.sqrt(1.0 + 4*tc**2))
-        r = pn + ((tc-1)/tn)*(pn - pc)
+        tn = 0.5 * (1.0 + np.sqrt(1.0 + 4 * tc ** 2))
+        r = pn + ((tc - 1) / tn) * (pn - pc)
         tc = tn
         pc = pn
 
-    x = b - l*W*div(pc)
+    x = b - l * W * div(pc)
     if bc:
         Pc(x, xmin, xmax)
 
     return x
 
 
+
+class TVProximal:
+    def __init__(self, D, DT, dim, projectC=None, vectorial=False, iterations=50):
+        """Compute a (generalised) proximal for a TV norm.
+
+        Should handle
+        - different gradients and divergences, as long as they
+          satisfy proper duality relations.
+        - mainly interested in the cases:
+            TV_l(x) = ||l(s)D u(s)||
+          as well as the "other" weighted:
+            TV^l(x) = ||D[l(s)x(s)]||
+
+        The problem to sole is min ||x-b|^2 + 2 gamma ||K x||
+        with K being D, or D.l or l.D
+
+        In both cases, l will be a weight function Omega -> R_+, l(s)
+        being the weight at pixel/voxel s in Omega, the signal domain,
+        its codomain being the positive reals.
+        When l is constant, this is the classical proximal.
+
+        Closed convex set constraint for signal u are also possible.
+        Two algorithms are proposed: A FISTA-type one and a simpler
+        Gradient projection method, slower, but much less memory
+        requiring fixed-point algorithm.
+
+        This implements the algorithms proposed in Beck and Teboulle
+        "Fast Gradient-Based Algorithms for Constrained Total Variation
+        Image Denoising and Deblurring Problems" with slight generalisation
+        regarding the operators, so that some explicit operator norms should
+        be provided.
+        For instance, when D is the standard discrete forward 2D gradient
+        its operator square-norm is (bounded by) 8, 12 for the 3D case.
+        When using D.l, or l.D, their square operator norms are bounded by
+          + 8 sup_s l(s)^2, for a 2D problem
+          + 12 sup_s l(s)^2 for a 3D problem.
+        Actually, in the case of "outer weight", the problem is handled
+        slightly differently by letting l be the radius field for the
+        balls where the dual variable has to be projected onto.
+
+        Dual variable values might be initialised to 0 at each solver call,
+        or kept, which could be important for some complex iterative
+        problems performances.
+
+        One or more callback functions can be added, they should
+        have the signature callback(x: numpy array, k : int) -> None.
+
+        ----------------------------------------------------------
+
+        The class encapsulates proximal calculations, following two
+        similar algorithms. This looks rather messy to me...
+
+        I could parametrise the dual variable projection, maybe OK,
+        maybe worse?
+
+        Parameters:
+        -----------
+        D : function-like
+            gradient operator
+        DT : function-like
+            adjoint gradient a.k.a. minus divergence operator.
+        dim : int
+            2 for a planar problem, 3 for a 3D one.
+        projectC : function-like, optional
+            projection onto convex set. If None, no projection
+            Pc must be an in-place projector. The default in None.
+        vectorial : bool, optional
+            if True, signal data is interpreted as vector-valued,
+            and scalar value otherwise. The default is False.
+        iterations : int, optional
+            number of iterations of the algorithm. The default is 50.
+        """
+        self._gamma = 1.0
+        self.iterations = iterations
+        self.D = D
+        self.DT = DT
+        self.dim = dim
+        self.vectorial = vectorial
+        self.omega = 8 if dim == 2 else 12
+        self._algorithm = 'fista'
+        self.callbacks = []
+        self._reset_dual_variable = True
+        self.phi = None
+        self.Pc  = None
+        self.init_Pc(projectC)
+        self.l = 1
+        self.inner = None
+        # Other projections could be used, to change the norm used in TV...
+        self.Pp = project_on_field_of_balls if not self.vectorial else project_on_field_of_Frobenius_balls
+        self.i = None
+        self.x = None
+
+    def set_spatial_weight(self, l, inner, sup_norm_l2=-1):
+        """Set the weight field l(s).
+
+        If l is "inner", i.e., we use D.l and not l.D
+        An estimate of ||D.l||^2 is needed. It is easily shown that
+        ||D.l||^2 \leq ||D|| 2 ||l||^2_\infinite.
+        """
+        self.l = l
+        if isinstance(l, np.ndarray):
+            self.inner = inner
+            if self.inner:
+                if sup_norm_l2 > 0:
+                    self.omega *= sup_norm_l2
+                else:
+                    self.omega *= l.max()**2
+        else:
+            self._gamma *= l
+
+    def init_Pc(self, func):
+        """Initialise the orthogonal projection on C.
+
+        if func is None, no projection.
+        """
+        if func is not None:
+            self.Pc = func
+        else:
+            def P_all(dummy_variable):
+                pass
+            self.Pc = P_all
+
+    def Pp_function(self):
+        """Initialise the dual variable projection.
+        """
+        if not self.inner:
+            def f(x):
+                self.Pp(x, radius=self.l)
+            return f
+        else:
+            def f(x):
+                self.Pp(x, radius=1.0)
+            return f
+
+    @property
+    def algorithm(self):
+        return self._algorithm
+
+    @algorithm.setter
+    def algorithm(self, method_name):
+        if method_name is not in ('fista', 'gp')
+            raise ValueError(f'algorith should either be "fista" or "gp" (gradient-projection)')
+        self._algorithm = method_name
+
+    def add_callback(self, callback):
+        self.callbacks.append(callback)
+
+    def run_callbacks(self):
+        """ Run the registered callback."""
+        for callback in self.callbacks:
+            callback(self.x, self.i)
+
+    @property
+    def reset_dual_variable(self):
+        return self._reset_dual_variable
+
+    @reset_dual_variable.setter
+    def reset_dual_variable(self, yesno):
+        self._reset_dual_variable = yesno
+
+    def create_dual_variable(self, b):
+        self.phi = np.zeros(b.shape + (self.dim,), dtype=b.dtype)
+
+    def _run_fista(self, b):
+        """Run a FISTA scheme. """
+        if self.reset_dual_variable or self.phi is None:
+            self.create_dual_variable(b)
+        psi = self.phi.copy()
+        previous_phi = self.phi.copy()
+        t = 1.0
+        L = self.omega * self._gamma
+
+        self.x = b.copy()
+
+        for self.i in range(self.iterations):
+            self.x[...] = b - self._gamma * self.DT(psi)
+            self.Pc(self.x)
+
+            self.run_callbacks()
+
+            self.phi[...] = self.DT(self.x)/L + psi
+            self.Pp(self.phi)
+
+            t_next = (1 + sqrt(1 + 4*t))/2
+            theta = ((t - 1)/t_next)
+
+            psi[...] = (1 + theta) * self.phi - theta * previous_phi
+            previous_phi = self.phi
+            t = t_next
+        self.x = b - self._gamma * self.DT(self.phi)
+        self.Pc(self.x)
+        return self.x
+
+    def _run_gp(self, b):
+        """Run a gradient projection scheme."""
+        if self.reset_dual_variable or self.phi is None:
+            self.create_dual_variable(b)
+
+        self.x = b.copy()
+
+        for self.i in range(self.iterations):
+            self.x[...] = b - self._gamma * self.DT(self.phi)
+            self.Pc(self.x)
+            L = self.omega * self._gamma
+
+            self.run_callbacks()
+
+            self.phi[...] += self.DT(self.x) / L
+            self.Pp(self.phi)
+
+        self.x = b - self._gamma * self.DT(self.phi)
+        self.Pc(self.x)
+        return self.x
+
+    def run(self, b):
+        if self._algorithm == 'fista':
+            return self._run_fista(b)
+        return self._run_gp(b)
+
+
+
 if __name__ == "__main__":
-    import  matplotlib.pyplot as plt
+    import matplotlib.pyplot as plt
     from matplotlib import cm
     from skimage import data
     import matplotlib.gridspec as gridspec
@@ -211,7 +443,7 @@ if __name__ == "__main__":
     y = fistaTV(image, l2, 200, xmin=float(m), xmax=float(M))
     z = fistaTV(image, l3, 200, xmin=float(m), xmax=float(M))
 
-    fig, ax = plt.subplots(2,2)
+    fig, ax = plt.subplots(2, 2)
     ((axl, axx), (axy, axz)) = ax
     axl.imshow(image, cmap=cm.Greys_r)
     axl.set_title('Original')
@@ -229,10 +461,10 @@ if __name__ == "__main__":
     # I choose a disk at the center of the image
     # of radius size/sqrt(2)
     dim1, dim2 = image.shape
-    x, y = np.mgrid[-1:1:dim1*1j, -1:1:dim2*1j]
+    x, y = np.mgrid[-1:1:dim1 * 1j, -1:1:dim2 * 1j]
     alpha, beta = 1.6, 0.4
     W = alpha * (x ** 2 + y ** 2 > 0.5).astype(float) + beta
-    omega = (W**2).max()
+    omega = (W ** 2).max()
 
     l1 = 5.
     l2 = 10.
@@ -247,7 +479,6 @@ if __name__ == "__main__":
     axw = fig.add_subplot(gs[1:3, 0])
     axw.imshow(W)
     axw.set_title(f"W field, out = {alpha:.01f}, in = {beta:.01f}", fontsize=24)
-
 
     axl = fig.add_subplot(gs[0:2, 1])
     axx = fig.add_subplot(gs[0:2, 2])
@@ -267,4 +498,3 @@ if __name__ == "__main__":
         s.axis('off')
 
     plt.show()
-
--- python/projectors.py
+++ python/projectors.py
@@ -16,15 +16,14 @@ TODO: some could run with a jit numba multicore....?
 from functools import reduce
 from operator import mul
 import numpy as np
-from label_simplex_fields import simplex_to_label_field, label_to_simplex_field
 
 __version__ = "0.0.1"
 __author__ = "François Lauze"
 
 
-
 def product_dimensions(a):
     return reduce(mul, a, 1)
+
 
 def partial_dimensions(a, k):
     """
@@ -34,7 +33,7 @@ def partial_dimensions(a, k):
 
 
 def project_on_field_of_balls(phi, radius=1.0):
-   """
+    """
    Project a vector field on balls field of given radius.
    if radius is an array, it should have the same non vectorial
    dimensions as phi, otherwise it will crash. phi is modified in-place
@@ -49,28 +48,28 @@ def project_on_field_of_balls(phi, radius=1.0):
    Returns:
    --------
        phi (also modified in-place)
-   """    
-   base_shape = phi.shape[:-1]
-   base_size = partial_dimensions(phi, -1)
-   K = phi.shape[-1]
-   
-   if type(radius) is np.ndarray:    
-       radius_shape = radius.shape
-       radius.shape = radius.size
-   
-   phi.shape = (base_size, K)
-   nphi = np.linalg.norm(phi, axis=1)
-   np.place(nphi, nphi <= radius, 1.0)
-   np.copyto(nphi, radius/nphi, where=nphi > radius)
-   nphi.shape = (-1,1)
-   phi *= nphi
-   phi.shape = base_shape + (K,)
-   if type(radius) is np.ndarray:
-       radius.shape = radius_shape
-       
-       
-def project_on_field_of_Frobenius_balls(phi, radius=1.0):
    """
+    base_shape = phi.shape[:-1]
+    base_size = partial_dimensions(phi, -1)
+    K = phi.shape[-1]
+
+    if type(radius) is np.ndarray:
+        radius_shape = radius.shape
+        radius.shape = radius.size
+
+    phi.shape = (base_size, K)
+    norm_phi = np.linalg.norm(phi, axis=1)
+    np.place(norm_phi, norm_phi <= radius, 1.0)
+    np.copyto(norm_phi, radius / norm_phi, where=norm_phi > radius)
+    norm_phi.shape = (-1, 1)
+    phi *= norm_phi
+    phi.shape = base_shape + (K,)
+    if type(radius) is np.ndarray:
+        radius.shape = radius_shape
+
+
+def project_on_field_of_Frobenius_balls(phi, radius=1.0):
+    """
    Project a vector field on matrix balls field of given radius.
    if radius is an array, it should have the same non matricial
    dimensions as phi, otherwise it will crash. phi is modified in-place
@@ -86,27 +85,27 @@ def project_on_field_of_Frobenius_balls(phi, radius=1.0):
    Returns:
    --------
        phi (also modified in-place)
-   """    
-   base_shape = phi.shape[:-2]
-   matrix_shape = phi.shape[-2:]
-   base_size = product_dimensions(base_shape)
-   matrix_size = product_dimensions(matrix_shape)
-   
-   if type(radius) is np.ndarray:    
-       radius_shape = radius.shape
-       radius.shape = radius.size
-   
-   phi.shape = (base_size, matrix_size)
-   nphi = np.linalg.norm(phi, axis=1)
-   np.place(nphi, nphi <= radius, 1.0)
-   np.copyto(nphi, radius/nphi, where=nphi > radius)
-   nphi.shape = (-1,1)
-   phi *= nphi
-   phi.shape = base_shape + matrix_shape
-   if type(radius) is np.ndarray:
-       radius.shape = radius_shape
-       
-       
+   """
+    base_shape = phi.shape[:-2]
+    matrix_shape = phi.shape[-2:]
+    base_size = product_dimensions(base_shape)
+    matrix_size = product_dimensions(matrix_shape)
+
+    if type(radius) is np.ndarray:
+        radius_shape = radius.shape
+        radius.shape = radius.size
+
+    phi.shape = (base_size, matrix_size)
+    nphi = np.linalg.norm(phi, axis=1)
+    np.place(nphi, nphi <= radius, 1.0)
+    np.copyto(nphi, radius / nphi, where=nphi > radius)
+    nphi.shape = (-1, 1)
+    phi *= nphi
+    phi.shape = base_shape + matrix_shape
+    if type(radius) is np.ndarray:
+        radius.shape = radius_shape
+
+
 def project_on_simplex_field(y):
     """
     classical projection into standard simplex algorithm.
@@ -116,24 +115,23 @@ def project_on_simplex_field(y):
     Parameters:
     ---------
     y: numpy array
-        array to be projected, size (m1, m2, K) or (m1, m2, m3, K) where K 
+        field to be projected, size (m1, m2, K) or (m1, m2, m3, K) where K
         is the simplex embedding dimension.
     Returns:
     --------
     z : numpy array:
-        projected array, same size as y.
+        projected field, same size as y.
     """
     base_shape = y.shape[:-1]
     base_size = partial_dimensions(y, -1)
     K = y.shape[-1]
     y.shape = (base_size, K)
     z = np.sort(y, axis=1)
-    z = (np.cumsum(z[:,::-1], axis=1)-1)/np.arange(1,K+1)
-    z = np.max(z, axis=1).reshape(base_size,1)
+    z = (np.cumsum(z[:, ::-1], axis=1) - 1) / np.arange(1, K + 1)
+    z = np.max(z, axis=1).reshape(base_size, 1)
     # ellipsis notation ensures that y is overwritten!
-    y[...] = np.maximum(y-z,0)
+    y[...] = np.maximum(y - z, 0)
     y.shape = base_shape + (K,)
-    
 
 
 def project_on_simplex_vertex(v):
@@ -143,45 +141,17 @@ def project_on_simplex_vertex(v):
     Parameters:
     ----------
     v: numpy array
-        array of standard simplex valued vectors
+        field of standard simplex valued vectors
     Returns:
     -------
     None
     """
-    vshape = v.shape
+    v_shape = v.shape
     base_dim, k = v.shape[:-1], v.shape[-1]
-    Ik = np.eye(k)
-    bdim = product_dimensions(base_dim)
-    v.shape = (bdim, k)
+    b_dim = product_dimensions(base_dim)
+    v.shape = (b_dim, k)
     Ik = np.eye(k)
     # ellipsis notation ensures that v is overwritten!
     v[...] = Ik[np.argmax(v, axis=1)]
-    v.shape = vshape
+    v.shape = v_shape
 
-
-
-
-
-
-def hmmf_binarise(v):
-    """
-    projection of vectors that are in a standard K-simplex i.e, K-finite 
-    probability fields, aka Hidden Markov Measure Fields (HMMF) to closest 
-    binary HHMF -- i.e., project each probability vector to the closest 
-    Dirac/vertex
-
-    Parameters
-    ----------
-    v : float(32) ndarray
-        y is an array of dimensionss base + (K,), where  K is the simplex
-        embedding dimension.
-
-    Returns
-    -------
-    z : float(32) ndarray. Projected array, same size as v
-    """
-    # Someof my C++ code should be simpler?
-    return label_to_simplex_field(simplex_to_label_field(v))
-    
-    
-    
